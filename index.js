65236701a110b7c85de2b09ea1046c493640d6c1
Ben West <benwest0291@outlook.com>
2021-09-16 15:14:59 +0100

    Radio cards challenge with HTML CSS JS

---
+++ index.js
@@ -0,0 +1,15 @@
+const radioMainContent = document.querySelector(".station-line");
+
+fetch("https://teclead.de/recruiting/radios")
+  .then((resp) => resp.json())
+  .then((data) => {
+    data.radios.map((radio) => {
+      const content = `
+      <div class="station-line">
+       <h2 class="station-name">${radio.name}</h2>
+         <img src="${radio.image}">
+       <h2 class="station-name">${radio.frequency}</h2>
+      </div>`;
+      radioMainContent.insertAdjacentHTML("afterend", content);
+    });
+  });
